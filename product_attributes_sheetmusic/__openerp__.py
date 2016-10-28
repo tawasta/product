b213b508a1d6e080dfcd6d67d64943aa4bbed374
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2016-10-28 11:25:55 +0300

    Fix a field type

--- product_attributes_sheetmusic/__openerp__.py
+++ product_attributes_sheetmusic/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Product sheet music attributes',
     'summary': 'Sheet music attributes for products',
-    'version': '8.0.0.1.10',
+    'version': '8.0.0.1.11',
     'category': 'Product',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
