1c528021b2591c088127e08213ed199216fe6ecd
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-04-27 15:07:32 +0300

    Add a sequence for product attributes

--- product_simplification/__openerp__.py
+++ product_simplification/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Product simplification',
     'summary': 'Simplified product views',
-    'version': '8.0.0.2.1',
+    'version': '8.0.0.2.2',
     'category': 'Sales',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
