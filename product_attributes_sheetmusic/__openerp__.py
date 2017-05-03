904f4d440f814373a80b4a2aace4763d5a5495cb
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-05-03 14:51:58 +0300

    Remove a readonly forcing

--- product_attributes_sheetmusic/__openerp__.py
+++ product_attributes_sheetmusic/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Product sheet music attributes',
     'summary': 'Sheet music attributes for products',
-    'version': '8.0.0.2.5',
+    'version': '8.0.0.2.6',
     'category': 'Product',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
