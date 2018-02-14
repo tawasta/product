386556534261329a1d21a5178d67d7e575035610
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-02-14 14:50:27 +0200

    New module product_suppleirinfo_for_customer_stock

---
+++ product_supplierinfo_for_customer_stock/models/stock_pack_operation.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+class StockPackOperation(models.Model):
+
+    _inherit = 'stock.pack.operation'
+
+    @api.multi
+    def _get_customer_product_info(self):
+        for pack_operation in self:
+            for partner_info in pack_operation.product_id.customer_ids:
+                if partner_info.name == pack_operation.picking_id.partner_id:
+                    pack_operation.customer_product_code = partner_info.product_code
+                    pack_operation.customer_product_name = partner_info.product_name
+
+    customer_product_code = fields.Char(
+        compute='_get_customer_product_info',
+        string='Customer Product Code'
+    )
+
+    customer_product_name = fields.Char(
+        compute='_get_customer_product_info',
+        string='Customer Product Name'
+    )    
