089fcb49c465e2e3931d3553790d508d984a25d8
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-03-22 20:30:17 +0200

    Improve formatting

--- product_vendor_codes_field/models/product_template.py
+++ product_vendor_codes_field/models/product_template.py
@@ -8,11 +8,13 @@ class ProductProduct(models.Model):
 
     @api.depends('seller_ids', 'seller_ids.product_code')
     def _get_vendor_codes(self):
-
         for record in self:
-            record.vendor_codes = ', '.join([seller.product_code for seller \
-                                             in record.seller_ids if seller.product_code]) or ''
+            record.vendor_codes = ', '.join([seller.product_code for seller
+                                             in record.seller_ids
+                                             if seller.product_code]) or ''
 
-    vendor_codes = fields.Char(compute='_get_vendor_codes',
-                               string='Partner Codes',
-                               store=True)
+    vendor_codes = fields.Char(
+        compute='_get_vendor_codes',
+        string='Partner Codes',
+        store=True
+    )
