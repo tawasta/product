e73d557aacebbaf024313adc519a1e442f1dc2a6
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-06-02 20:20:59 +0300

    Add new module for marking analytic tags for product or product category

---
+++ product_analytic_tags/models/product_product.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+class ProductProduct(models.Model):
+
+    _inherit = 'product.product'
+
+    # Allow calling get_analytic_tags from product template
+    def get_analytic_tags(self):
+        return self.product_tmpl_id.get_analytic_tags()
---
+++ product_analytic_tags/models/product_template.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+class ProductTemplate(models.Model):
+
+    _inherit = 'product.template'
+
+    analytic_tag_ids = fields.Many2many(
+        comodel_name='account.analytic.tag',
+        string='Analytic tags',
+    )
+
+    def get_analytic_tags(self):
+        # The return won't work with multiple records
+        self.ensure_one()
+
+        analytic_tags = self.analytic_tag_ids \
+                        or self.categ_id.get_analytic_tags()
+
+        return analytic_tags
