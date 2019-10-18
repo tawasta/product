02c8f0a88cb122f3d0278f55ff8fb15df998106b
difbit <timo.kekalainen@hotmail.com>
2019-10-18 11:42:55 +0300

    [ADD] Pytho file

---
+++ product_default_location/models/product_template.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+
+    _inherit = 'product.template'
+
+    product_default_location = fields.Many2one(
+        'stock.location',
+        string='Default Location',
+    )
