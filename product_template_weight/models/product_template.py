91f57f2285e71c5ce11c92d7b5ec84fe93740fc5
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-07-08 15:53:50 +0300

    [ADD] Create support

--- product_template_weight/models/product_template.py
+++ product_template_weight/models/product_template.py
@@ -7,17 +7,20 @@ class ProductTemplate(models.Model):
 
     @api.model
     def create(self, values):
-
-        return super(ProductTemplate, self).create(values)
+        res = super(ProductTemplate, self).create(values)
+        res._update_variant_weight_and_volume(values)
+        return res
 
     @api.multi
     def write(self, values):
+        res = super(ProductTemplate, self).write(values)
+        self._update_variant_weight_and_volume(values)
+        return res
 
+    def _update_variant_weight_and_volume(self, values):
         for record in self:
             if 'weight' in values:
                 record.product_variant_ids.write({'weight': values['weight']})
 
             if 'volume' in values:
                 record.product_variant_ids.write({'volume': values['volume']})
-
-        return super(ProductTemplate, self).write(values)
