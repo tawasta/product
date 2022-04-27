2d73aae14815a59a042e0ab10b47381f139bf92d
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2022-04-27 09:05:53 +0000

    12.0 dev product on hand qty color fix search

--- product_on_hand_qty_color/models/product_template.py
+++ product_on_hand_qty_color/models/product_template.py
@@ -13,7 +13,7 @@ class ProductTemplate(models.Model):
     def _compute_stock_on_several_locations(self):
         for product in self:
             quants = self.env['stock.quant'].sudo().search([
-                ('product_id', '=', product.id)])
+                ('product_tmpl_id', '=', product.id)])
 
             locations = [q.location_id.id for q in quants
                          if q.location_id.usage == 'internal']
