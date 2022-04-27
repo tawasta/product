56295074a52ed914a7ec0be848f3adb415c97fb1
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2022-04-27 06:16:11 +0000

    12.0 dev product on hand qty color

---
+++ product_on_hand_qty_color/static/src/js/web_on_hand.js
@@ -0,0 +1,21 @@
+odoo.define('widget_on_hand_color', function (require) {
+    "use strict";
+
+var FormRenderer = require('web.FormRenderer');
+
+FormRenderer.include({
+    _renderButtonBox: function (node) {
+        var $button = this._super.apply(this, arguments);
+        var length = node.children.length;
+        var child;
+        for (child = 0; child < length; child++) {
+            if ($button[0].children[child].name === "action_open_quants" &&
+                    this.state.data.has_stock_on_several_locations === true) {
+                $button[0].children[child].style.color = "brown";
+            }
+        }
+        return $button;
+    }
+});
+
+});
