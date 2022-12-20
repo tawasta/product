ef1e1a218bbb846432a85262dfcf64b710e796a2
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2022-12-20 07:20:12 +0000

    [ADD] A new module product_archive_product_archive_mrp_parameter

---
+++ product_archive_product_archive_mrp_parameter/models/product_product.py
@@ -0,0 +1,25 @@
+
+from odoo import api, models
+
+
+class ProductProduct(models.Model):
+
+    _inherit = 'product.product'
+
+    def area_parameter_search(self, product):
+        """MRP area Parameter generator"""
+        parameters = self.env['product.mrp.area'].sudo().search([
+            ('product_id', '=', product.id), '|',
+            ('active', '=', True), ('active', '=', False)
+        ])
+        for parameter in parameters:
+            yield parameter
+
+    @api.multi
+    def toggle_active(self):
+        """Switch parameters as archived"""
+        res = super(ProductProduct, self).toggle_active()
+        for product in self:
+            for parameter in self.area_parameter_search(product):
+                parameter.active = product.active
+        return res
