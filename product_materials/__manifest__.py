82059b241e41150a031bc90a22b75144fea14347
difbit <timo.kekalainen@tawasta.fi>
2023-06-07 14:58:16 +0300

    [IMP] product_materials module: added attachment option

--- product_materials/__manifest__.py
+++ product_materials/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Product Materials",
     "summary": "Product Materials model to be used on products",
-    "version": "14.0.1.0.2",
+    "version": "14.0.1.0.3",
     "category": "Product",
     "website": "https://gitlab.com/tawasta/odoo/product",
     "author": "Tawasta",
