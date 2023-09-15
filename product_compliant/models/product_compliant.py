b51abbc50d3f56bac472a52cf92115940f26a37f
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2023-09-15 13:38:06 +0000

    [MIG] Migration of product_default_location module from 12.0 to 14.0 version

--- product_compliant/models/product_compliant.py
+++ product_compliant/models/product_compliant.py
@@ -1,8 +1,9 @@
-from odoo import models, fields
+from odoo import fields, models
+
 
 class ProductCompliant(models.Model):
-    _name = 'product.compliant'
-    _description = 'Product Compliant'
+    _name = "product.compliant"
+    _description = "Product Compliant"
 
-    name = fields.Char('Name', required=True)
-    description = fields.Text('Description')
+    name = fields.Char("Name", required=True)
+    description = fields.Text("Description")
--- product_materials/models/product_template.py
+++ product_materials/models/product_template.py
@@ -1,14 +1,22 @@
-from odoo import fields, models, api
+from odoo import fields, models
 
 PRODUCT_DOMAIN = "[('category.code', '=', 'product')]"
 PURCHASE_DOMAIN = "[('category.code', '=', 'purchase')]"
+
+
 class ProductTemplate(models.Model):
 
     _inherit = "product.template"
 
+    material = fields.Many2many(
+        "product.material", string="Material", domain=PRODUCT_DOMAIN
+    )
 
-    material = fields.Many2many("product.material", string="Material", domain=PRODUCT_DOMAIN)
-
-    material_purchase = fields.Many2many(comodel_name="product.material", relation="rel_purchase_material", string="Material", domain=PURCHASE_DOMAIN)
+    material_purchase = fields.Many2many(
+        comodel_name="product.material",
+        relation="rel_purchase_material",
+        string="Material",
+        domain=PURCHASE_DOMAIN,
+    )
 
     material_purchase_text = fields.Char(string="Material text")
