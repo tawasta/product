36f867fc264e95c9a871087c850e65a994c938da
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2023-11-15 13:28:20 +0000

    Revert "Merge branch '14.0-hotfix_product_variant_internal_note_page' into '14.0'"
    This reverts merge request !198

--- product_variant_internal_note/__manifest__.py
+++ product_variant_internal_note/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Internal note for product variant",
     "summary": "Internal note for product variant",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "Product",
     "website": "https://gitlab.com/tawasta/odoo/product",
     "author": "Tawasta",
