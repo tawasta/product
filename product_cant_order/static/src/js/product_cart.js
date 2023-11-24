744ee741e9f6bcaf5274aacab0e3c686b579aa00
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-11-24 07:41:16 +0200

    Run pre commit all files

--- product_cant_order/static/src/js/product_cart.js
+++ product_cant_order/static/src/js/product_cart.js
@@ -1,15 +1,17 @@
-odoo.define("product_cant_order.product", function () {
+odoo.define("product_cant_order.product", function (require) {
     "use strict";
 
-    $("input[type=hidden][class='product_id']").bind("change", function () {
-        var product_id = $(this).val();
-        if (product_id) {
-            var action = "/check/product/" + product_id;
-            $.post(action, function (res) {
-                var results = JSON.parse(res);
-                if (results.error) {
-                    console.error(results.error);
-                } else if (results.order_status === true) {
+    var ajax = require("web.ajax");
+
+    function checkProductAvailability(productId, callback) {
+        var action = "/check/product/" + productId;
+        ajax.jsonRpc(action, "call", {product_id: productId}).then(function (results) {
+            if (results.error) {
+                console.error(results.error);
+                callback(false);
+            } else {
+                callback(results.order_status === true);
+                if (results.order_status === true) {
                     $("#add_to_cart").removeClass().addClass("d-none");
                 } else {
                     $("#add_to_cart")
@@ -18,8 +20,37 @@ odoo.define("product_cant_order.product", function () {
                             "btn btn-primary btn-lg mt16 js_check_product a-submit d-block d-sm-inline-block"
                         );
                 }
-                $.unblockUI();
-            });
+            }
+        });
+    }
+
+    function runCheck() {
+        if ($(".oe_website_sale").length && $("#add_to_cart").length) {
+            var productId = $(".product_id").val();
+            if (productId) {
+                // eslint-disable-next-line no-unused-vars
+                checkProductAvailability(productId, function (isAvailable) {
+                    // Tässä voi toteuttaa lisätoimintoja riippuen isAvailable-arvosta
+                });
+            }
         }
+    }
+
+    $(document).ready(runCheck);
+
+    // Tapahtumankäsittelijä .product_id kentälle
+    $(".product_id").on("change", function () {
+        runCheck();
     });
+
+    // Oletusarvoinen paikka, jossa .product_id kentän arvoa muutetaan ohjelmallisesti
+    // Tämän koodin sijainti riippuu siitä, missä ja milloin arvoa tarvitsee muuttaa
+    function updateProductIdAndTriggerChange(newProductId) {
+        $(".product_id").val(newProductId).trigger("change");
+    }
+
+    return {
+        checkProductAvailability: checkProductAvailability,
+        updateProductIdAndTriggerChange: updateProductIdAndTriggerChange,
+    };
 });
