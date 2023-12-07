1fb19a78331af93f300674db55a394f7f4d91877
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2023-12-07 12:37:20 +0000

    [ADD] Renamed product_template_weight module to product_template_update_variant_volume

--- product_template_update_variant_volume/models/product_template.py
+++ product_template_update_variant_volume/models/product_template.py
@@ -8,18 +8,15 @@ class ProductTemplate(models.Model):
     @api.model
     def create(self, values):
         res = super(ProductTemplate, self).create(values)
-        res._update_variant_weight_and_volume(values)
+        res._update_variant_volume(values)
         return res
 
     def write(self, values):
         res = super(ProductTemplate, self).write(values)
-        self._update_variant_weight_and_volume(values)
+        self._update_variant_volume(values)
         return res
 
-    def _update_variant_weight_and_volume(self, values):
+    def _update_variant_volume(self, values):
         for record in self:
-            if "weight" in values:
-                record.product_variant_ids.write({"weight": values["weight"]})
-
             if "volume" in values:
                 record.product_variant_ids.write({"volume": values["volume"]})
