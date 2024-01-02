41ed9af3a4f249fde93b00dde0ffef8d9ba235f2
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2024-01-02 11:14:53 +0200

    Run pre commit all files

--- product_inventory/models/stock_inventory.py
+++ product_inventory/models/stock_inventory.py
@@ -1,8 +1,7 @@
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class StockInventoryLine(models.Model):
     _inherit = "stock.inventory.line"
 
     adjustment_date = fields.Datetime(related="inventory_id.date")
-
