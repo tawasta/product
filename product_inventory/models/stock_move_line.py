5c64052c8be29118a29323d523d52a2f194ba071
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2024-01-12 08:23:38 +0000

    [FIX] product_inventory module: a partial done to prevent errors in some

--- product_inventory/models/stock_move_line.py
+++ product_inventory/models/stock_move_line.py
@@ -5,9 +5,13 @@ class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
     def _action_done(self):
-        res = super()._action_done()
-
+        # Needs a better way to do this, right? It will not fetch the latest
+        # information but it cannot be run after "res = super()._action_done()"
+        # because it would cause an error upon unbuilds in manufacturing orders
+        # or when their produced quantity is changed after their initial assigned
+        # quantity.
         for record in self:
             record.sudo().product_id._compute_stock_date()
 
+        res = super()._action_done()
         return res
