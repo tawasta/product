d875e4b071bd1699cfaf12ac63ec5c27e04d684d
difbit <timo.kekalainen@tawasta.fi>
2024-02-23 13:58:07 +0200

    [IMP] product_internal_comments module updates for pre-commit

--- product_internal_comments/__manifest__.py
+++ product_internal_comments/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Internal Comments for Products",
     "summary": "New comments field for product",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "category": "Product",
     "website": "https://gitlab.com/tawasta/odoo/product",
     "author": "Tawasta",
