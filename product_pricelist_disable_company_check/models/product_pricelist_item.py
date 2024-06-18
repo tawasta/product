da0ffd24efc977dc6113f5c72e89488aec320963
difbit <timo.kekalainen@tawasta.fi>
2024-06-18 09:16:59 +0300

    [MIG] Migration of product_pricelist_disable_company_check module from 14.0 to 17.0 version

---
+++ product_pricelist_disable_company_check/models/product_pricelist_item.py
@@ -0,0 +1,8 @@
+from odoo import fields, models
+
+
+class PricelistItem(models.Model):
+    _inherit = "product.pricelist.item"
+
+    product_tmpl_id = fields.Many2one(check_company=False)
+    product_id = fields.Many2one(check_company=False)
