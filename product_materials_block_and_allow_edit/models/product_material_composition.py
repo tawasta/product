a801ba0e95706b2bf3fde186bea5df8ecd386e28
difbit <timo.kekalainen@tawasta.fi>
2024-07-08 15:12:16 +0300

    [IMP] product_materials_block_and_allow_edit module: Check multiple materials and allow to edit upper categories

--- product_materials_block_and_allow_edit/models/product_material_composition.py
+++ product_materials_block_and_allow_edit/models/product_material_composition.py
@@ -7,13 +7,15 @@ class ProductMaterialComposition(models.Model):
     _inherit = "product.material.composition"
 
     def write(self, vals):
-        allow_material_edit = self.product_product_id.allow_material_edit
+        for material in self:
+            allow_material_edit = material.product_product_id.allow_material_edit
 
-        edit_delivery_package = vals.get("is_delivery_package", False)
+            edit_delivery_package = vals.get("is_delivery_package", False)
+            edit_upper_category = vals.get("product_material_upper_category_id", False)
 
-        if allow_material_edit or edit_delivery_package:
-            return super().write(vals)
-        else:
-            raise ValidationError(
-                _("Editing materials is not enabled from its product")
-            )
+            if allow_material_edit or edit_delivery_package or edit_upper_category:
+                return super().write(vals)
+            else:
+                raise ValidationError(
+                    _("Editing materials is not enabled from its product")
+                )
