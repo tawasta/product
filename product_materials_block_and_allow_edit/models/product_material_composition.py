5dc8b0ccdf60252234df578e48e0756ae0d2e7d0
difbit <timo.kekalainen@tawasta.fi>
2024-07-10 09:15:22 +0300

    [IMP] product_materials_block_and_allow_edit module: Allow and disallow editing of materials with a user group

--- product_materials_block_and_allow_edit/models/product_material_composition.py
+++ product_materials_block_and_allow_edit/models/product_material_composition.py
@@ -7,15 +7,9 @@ class ProductMaterialComposition(models.Model):
     _inherit = "product.material.composition"
 
     def write(self, vals):
-        for material in self:
-            allow_material_edit = material.product_product_id.allow_material_edit
-
-            edit_delivery_package = vals.get("is_delivery_package", False)
-            edit_upper_category = vals.get("product_material_upper_category_id", False)
-
-            if allow_material_edit or edit_delivery_package or edit_upper_category:
-                return super().write(vals)
-            else:
-                raise ValidationError(
-                    _("Editing materials is not enabled from its product")
-                )
+        if self.env["res.users"].has_group(
+            "product_materials_block_and_allow_edit.allow_material_edit"
+        ):
+            return super().write(vals)
+        else:
+            raise ValidationError(_("Editing materials is not enabled for your user"))
