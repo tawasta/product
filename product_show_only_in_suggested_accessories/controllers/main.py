cf2406f6a92a87df41e2595045310c80bc5f0f0c
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2024-07-22 09:08:21 +0300

    [IMP] - modified module to 17 version

---
+++ product_show_only_in_suggested_accessories/controllers/main.py
@@ -0,0 +1,14 @@
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo.http import request, route
+
+
+class WebsiteSale(WebsiteSale):
+
+    def _shop_lookup_products(self, attrib_set, options, post, search, website):
+        # Kutsu alkuperäistä funktiota
+        fuzzy_search_term, product_count, search_product = super(WebsiteSale, self)._shop_lookup_products(attrib_set, options, post, search, website)
+        
+        # Lisää tuotteiden suodatus
+        search_product = search_product.filtered(lambda p: p.show_only_in_suggested_accessories == False)
+        
+        return fuzzy_search_term, product_count, search_product
