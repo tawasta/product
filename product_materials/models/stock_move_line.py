c0a8f48bc5735dd760872886ce342418852dea67
difbit <timo.kekalainen@tawasta.fi>
2025-03-18 08:31:28 +0200

    [MIG] View and PDF print fixes and changs

--- product_materials/models/stock_move_line.py
+++ product_materials/models/stock_move_line.py
@@ -6,41 +6,14 @@ class StockMoveLine(models.Model):
 
     def _get_aggregated_product_quantities(self, **kwargs):
         aggregated_move_lines = super()._get_aggregated_product_quantities(**kwargs)
-        for move_line in self:
-            name = move_line.product_id.display_name
-            description = move_line.move_id.description_picking
-            if description == name or description == move_line.product_id.name:
-                description = False
-            uom = move_line.product_uom_id
-            line_key = (
-                str(move_line.product_id.id)
-                + "_"
-                + name
-                + (description or "")
-                + "uom "
-                + str(uom.id)
-            )
 
-            materials = (
-                self.env["product.material"]
-                .sudo()
-                .search(
-                    [
-                        (
-                            "id",
-                            "in",
-                            move_line.move_id.product_id.product_material_composition_ids.product_material_id.ids,  # noqa: B950
-                        )
-                    ]
-                )
-            )
+        for aggregated_move_line in aggregated_move_lines:
 
-            show_materials = (
-                move_line.move_id.sale_line_id.order_id.show_materials_with_report
-            )
+            move_id = aggregated_move_lines[aggregated_move_line].get("move", False)
+            materials = move_id.product_id.product_material_composition_ids.product_material_id
+            show_materials = move_id.sale_line_id.order_id.show_materials_with_report
 
-            if line_key in aggregated_move_lines:
-                aggregated_move_lines[line_key]["materials"] = materials
-                aggregated_move_lines[line_key]["show_materials"] = show_materials
+            aggregated_move_lines[aggregated_move_line]["materials"] = materials
+            aggregated_move_lines[aggregated_move_line]["show_materials"] = show_materials
 
         return aggregated_move_lines
