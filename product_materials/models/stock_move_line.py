45f823651584ccd1655e636cd7dd74ed57cf7112
difbit <timo.kekalainen@tawasta.fi>
2025-03-18 09:39:09 +0200

    [MIG] product_materials module: Improved PDF prints and added translations

--- product_materials/models/stock_move_line.py
+++ product_materials/models/stock_move_line.py
@@ -8,12 +8,15 @@ class StockMoveLine(models.Model):
         aggregated_move_lines = super()._get_aggregated_product_quantities(**kwargs)
 
         for aggregated_move_line in aggregated_move_lines:
-
             move_id = aggregated_move_lines[aggregated_move_line].get("move", False)
-            materials = move_id.product_id.product_material_composition_ids.product_material_id
+            materials = (
+                move_id.product_id.product_material_composition_ids.product_material_id
+            )
             show_materials = move_id.sale_line_id.order_id.show_materials_with_report
 
             aggregated_move_lines[aggregated_move_line]["materials"] = materials
-            aggregated_move_lines[aggregated_move_line]["show_materials"] = show_materials
+            aggregated_move_lines[aggregated_move_line][
+                "show_materials"
+            ] = show_materials
 
         return aggregated_move_lines
