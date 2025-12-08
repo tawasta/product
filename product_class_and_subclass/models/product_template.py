082f20e7dba258f9ddedb7480f93d45320325774
difbit <timo.kekalainen@tawasta.fi>
2025-12-08 14:43:15 +0200

    [ADD] A new module: product_class_and_subclass. It adds class and subclass fields to a product

---
+++ product_class_and_subclass/models/product_template.py
@@ -0,0 +1,13 @@
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    product_class = fields.Char(
+        string="Class", store=True, copy=False, help="Assigned class of a product"
+    )
+
+    product_subclass = fields.Char(
+        string="Subclass", store=True, copy=False, help="Assigned subclass of a product"
+    )
