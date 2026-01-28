c8239c891a896abd07d319e07205839c2e66acb3
difbit <timo.kekalainen@tawasta.fi>
2026-01-28 12:41:32 +0200

    [IMP] Modified manifest file

--- product_profile_dimension_text/__manifest__.py
+++ product_profile_dimension_text/__manifest__.py
@@ -28,10 +28,6 @@
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "external_dependencies": {
-        "python": [],
-        "bin": [],
-    },
     "depends": [
         "product_diameter",
     ],
