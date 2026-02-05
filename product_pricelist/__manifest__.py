72398e07e06ae116f701a589ba6bafb41f723a28
difbit <timo.kekalainen@tawasta.fi>
2026-02-05 15:13:15 +0200

    [IMP] Removed external_dependencies

--- product_pricelist/__manifest__.py
+++ product_pricelist/__manifest__.py
@@ -28,7 +28,6 @@
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "external_dependencies": {"python": [], "bin": []},
     "depends": ["product"],
     "data": ["views/product_template_view.xml"],
     "demo": [],
