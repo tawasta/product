60e11ab60da6ba5adfd43e9c4fc7b47b61e0859d
difbit <timo.kekalainen@tawasta.fi>
2026-05-21 08:17:04 +0300

    [IMP] product_template_revision_code module: Minor version update

--- product_template_revision_code/__manifest__.py
+++ product_template_revision_code/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Revision Code of product template",
     "summary": "Add Revision Code -field to product template",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "category": "Product",
     "website": "https://github.com/tawasta/product",
     "author": "Futural",
